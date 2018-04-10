4b5bcb4f789a1737c41d067c1b55432903fa2162
omarbridgetech <omar99awaisha@gmail.com>
2018-04-10 10:37:47 +0300

    aboutus intent

--- app/src/main/java/com/ajeek/edu/attendancetracking/AboutUsActivity.java
+++ app/src/main/java/com/ajeek/edu/attendancetracking/AboutUsActivity.java
@@ -5,18 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-//Good morning!
-public class MainActivity extends AppCompatActivity {
+public class AboutUsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about_us);
     }
 
-    public void aboutus(View view) {
+    public void about(View view) {
         Intent intent = new Intent (this,AboutUsActivity.class);
 
-        startActivity(intent);
     }
 }
